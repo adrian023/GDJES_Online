4c2e873ce386b58bae24546cfa7a7618d9dfd547
adrian023 <azfabonan@iskolarngbayan.pup.edu.ph>
2023-09-23 10:06:37 +0800

    made the model for students

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -7,5 +7,54 @@ use Illuminate\Database\Eloquent\Model;
 
 class Student extends Model
 {
+    protected $table = 'students';
+    protected $fillable = [
+        'grade',
+        'email',
+        'password',
+        'lrn',
+        'psa_number',
+        'last_name',
+        'first_name',
+        'middle_name',
+        'extension_name',
+        'birth_date',
+        'age',
+        'gender',
+        'is_indigenous',
+        'indigenous_group',
+        'kinagisnang_wika',
+        'religion',
+        'has_special_condition',
+        'special_condition',
+        'address',
+        'barangay',
+        'city',
+        'father_last_name',
+        'father_first_name',
+        'father_middle_name',
+        'father_cell_no',
+        'mother_last_name',
+        'mother_first_name',
+        'mother_middle_name',
+        'mother_cell_no',
+        'guardian_last_name',
+        'guardian_first_name',
+        'guardian_middle_name',
+        'guardian_cell_no',
+        'is_4p',
+        'household_id',
+        'has_own_cellphone_and_tablet',
+        'has_computer',
+        'no_gadgets',
+        'has_tv',
+        'has_internet',
+        'mobile_data_only',
+        'distance_learning',
+    ];
+
+    protected $casts = [
+        'password' => 'hashed',
+    ];
     use HasFactory;
 }
